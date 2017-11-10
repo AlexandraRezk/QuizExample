3578b62ea279559e3c716da10a194c2970f3b5d7
Alexandra Rezk <alexandrarezk@gmail.com>
2017-11-10 15:32:16 -0500

    Updated Results

--- js/quiz.js
+++ js/quiz.js
@@ -15,19 +15,19 @@ document.getElementById("form1").onsubmit=function() {
 			break;
 			
 		case 25:
-			result2 = "I don't think you studied.<br/><img src='images/25.jpg' width='300'/>"
+			result2 = "You need to spend more time. Try again.<br/><img src='images/25.jpg' width='300'/>"
 			break;
 			
 		case 50:
-			result2 = "I don't think you studied.<br/><img src='images/50.jpg' width='300'/>"
+			result2 = "I think you could do better. Try again.<br/><img src='images/50.jpg' width='300'/>"
 			break;
 		
 		case 75:
-			result2 = "I don't think you studied.<br/><img src='images/75.jpg' width='300'/>"
+			result2 = "So close. Try again.<br/><img src='images/75.jpg' width='300'/>"
 			break;
 			
 		case 100:
-			result2 = "I don't think you studied.<br/><img src='images/100.jpg' width='300'/>"
+			result2 = "You're a Javascript pro!<br/><img src='images/100.jpg' width='300'/>"
 			break;
 				  }
 	/*//Try and change this to a switch statement. Will be much more effiecent
